daeb8b9a8e430fb83583bf108d18ed4fd51fe99a
Daniil Borovkov <daniilborowkow@gmail.com>
2018-12-05 02:42:29 +0600

    Ember app starter

--- app/routes/blockchain.js
+++ app/routes/blockchain.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model () {
+    return {}
+  },
+
+  init () {
+    this._super(...arguments)
+    console.log('Inited')
+  }
 });
